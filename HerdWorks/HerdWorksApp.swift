9d48eb413aa278979e773d3eaa52af1a668e7448
Paul Mooney <daishisystems@users.noreply.github.com>
2025-10-10 10:29:06 +0200

    Initial Commit

---
+++ HerdWorks/HerdWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerdWorksApp.swift
+//  HerdWorks
+//
+//  Created by Paul Mooney on 2025/10/10.
+//
+
+import SwiftUI
+
+@main
+struct HerdWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HerdWorksTests/HerdWorksTests.swift
@@ -0,0 +1,17 @@
+//
+//  HerdWorksTests.swift
+//  HerdWorksTests
+//
+//  Created by Paul Mooney on 2025/10/10.
+//
+
+import Testing
+@testable import HerdWorks
+
+struct HerdWorksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
